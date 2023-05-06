3270552f2498ebb7715e99a3c4c01444e7465967
ZyeByte <avalae.isadora@tutanota.com>
2023-05-06 03:12:31 +0200

    Add Projection

---
+++ source/msdf/projection.d
@@ -0,0 +1,52 @@
+module msdf.projection;
+
+import inmath;
+
+class Projection {
+public:
+    this() {
+        scale = vec2d(1);
+        translate = vec2d(0);
+    }
+
+    this(in vec2d scale, in vec2d translate) {
+        this.scale = scale;
+        this.translate = translate;
+    }
+
+    vec2d project(in vec2d coord) const {
+        return scale * (coord + translate);
+    }
+
+    vec2d unproject(in vec2d coord) const {
+        return coord / scale - translate;
+    }
+
+    vec2d projectVector(in vec2d vector) const {
+        return scale * vector;
+    }
+
+    vec2d unprojectVector(in vec2d vector) const {
+        return vector / scale;
+    }
+
+    double projectX(double x) const {
+        return scale.x * (x + translate.x);
+    }
+
+    double projectY(double y) const {
+        return scale.y * (y + translate.y);
+    }
+
+    double unprojectX(double x) const {
+        return x / scale.x - translate.x;
+    }
+
+    double unprojectY(double y) const {
+        return y / scale.y - translate.y;
+    }
+
+private:
+    vec2d scale;
+    vec2d translate;
+}
